cb91fe40e0080ee65905c30d1700db17fbfc9673
mohamed <mhalwasoffice@gmail.com>
2024-07-31 19:09:23 +0100

    updating

--- scrollinggsap/src/componants/header.jsx
+++ scrollinggsap/src/componants/header.jsx
@@ -1,10 +1,14 @@
 import "../../src/App.css";
-import { useState } from "react";
+import { useState, useRef } from "react";
+import gsap from "gsap";
+
 export default function HeaderSection() {
   const [Firstspan, setFirstSpan] = useState(0);
   const [opacity, setOpacity] = useState(1);
   const [translateY, setTranslateX] = useState(0);
   const [rotatingMen, setRotatingMenu] = useState(0);
+  const menuAnimation = useRef(null);
+
   let animated = false;
   function animtedMenu() {
     if (!animated) {
@@ -13,12 +17,42 @@ export default function HeaderSection() {
       setTranslateX(-10);
       setRotatingMenu(360);
       animated = true;
+      gsap.to(menuAnimation.current, {
+        opacity: 1,
+        ease: "power4",
+        x: -500,
+      });
+      gsap.to(menuAnimation.current.children, {
+        opacity: 1,
+        y: 0,
+        ease: "power4",
+        duration: 1,
+        stagger: {
+          amount: 1,
+          from: "top",
+        },
+      });
     } else {
       setFirstSpan(0);
       setOpacity(1);
       setTranslateX(0);
       setRotatingMenu(0);
       animated = false;
+      gsap.to(menuAnimation.current, {
+        opacity: 0,
+        ease: "power4",
+        x: 500,
+      });
+      gsap.to(menuAnimation.current.children, {
+        opacity: 0,
+        y: -100,
+        ease: "power4",
+        duration: 1,
+        stagger: {
+          amount: 1,
+          from: "top",
+        },
+      });
     }
   }
   return (
@@ -29,7 +63,7 @@ export default function HeaderSection() {
         onClick={animtedMenu}
         style={{
           transform: `rotate(${rotatingMen}deg)`,
-          transition : "all .6s ease"
+          transition: "all .6s ease",
         }}
       >
         <span
@@ -50,6 +84,12 @@ export default function HeaderSection() {
             transition: "all .4s ease",
           }}
         ></span>
+      </div>
+      <div class="leftSide" ref={menuAnimation}>
+        <h2>HOME</h2>
+        <h2>ABOUT</h2>
+        <h2>CONTACT</h2>
+        <h2>GITHUB</h2>
       </div>
     </header>
   );
